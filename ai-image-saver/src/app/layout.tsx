a480030b177041d21a622ef33db4d0a5016b8abe
Alon Bibring <alon.bibring@gmail.com>
2024-12-01 01:15:34 -0500

    refactoring font creation to its own file so in the future, it is easier to find/refactor

--- ai-image-saver/src/app/layout.tsx
+++ ai-image-saver/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
+
 import { Providers } from "./Providers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { geistMono, geistSans } from "./font";
 
 export const metadata: Metadata = {
   title: "AI Image Generator",
